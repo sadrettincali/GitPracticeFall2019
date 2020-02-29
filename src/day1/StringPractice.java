cc856da8b647ad03ca76d9c9c092dc76cc183cb4
sadrettincali <sadrettincali@gmail.com>
2020-02-29 15:50:45 -0500

    new

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,6 +3,7 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("Firt day of my life");
 
     }
 }
