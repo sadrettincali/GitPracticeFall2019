6a48c28488369e779f3d2f17bd45fa1824991a8c
sadrettincali <sadrettincali@gmail.com>
2020-03-11 20:19:26 -0400

    new

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -2,7 +2,7 @@ package day1;
 
 public class Colors {
     public static void main(String[] args) {
-        String[] colors = {"red", "blue", "yellow"};
+        String[] colors = {"red", "blue", "yellow", "green"};
 
 
     }
